841a2d5276e6f0d19b96dfeddd6cd305f57565fc
Emdad Hossain <emdadhossain25@users.noreply.github.com>
2025-11-01 15:11:26 +0600

    added a skeleton for the book list item

--- app/src/main/java/com/example/bookstoreapp/MainActivity.kt
+++ app/src/main/java/com/example/bookstoreapp/MainActivity.kt
@@ -22,28 +22,10 @@ class MainActivity : ComponentActivity() {
         setContent {
             BookStoreAppTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+
                 }
             }
         }
     }
 }
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    BookStoreAppTheme {
-        Greeting("Android")
-    }
-}
---
+++ app/src/main/java/com/example/bookstoreapp/ui/booklist/component/BookListItem.kt
@@ -0,0 +1,16 @@
+package com.example.bookstoreapp.ui.booklist.component
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
+@Composable
+fun BookListItem(modifier: Modifier = Modifier) {
+   //card
+        // row
+            // image
+            // column
+                // Book name
+                // Row
+                    // author name
+                    // rating
+}
